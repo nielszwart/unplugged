59be50a84a600bbfd8623aa60ca320977dab0ea6
Niels Zwart <n.zwart@goldrepublic.com>
2017-12-10 19:12:55 +0100

    Homepage setup

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,18 +4,21 @@
 namespace App\Controller;
 
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class IndexController extends BaseController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="homepage_en")
+     * @Route("/{_locale}", name="homepage_nl")
+     * @Method("GET")
      */
     public function number()
     {
         $number = mt_rand(0, 100);
 
-        return $this->render('homepage.twig', array(
+        return $this->render('website/homepage.twig', array(
             'number' => $number,
         ));
     }
